0c266936287e12475f819dfadb88f0a3e2c8b45a
Nastasya <Nenastye90@gmail.com>
2023-11-14 13:37:12 +0300

    Postman

---
+++ src/test/java/ru/netologe/rest/postmanechoTest.java
@@ -0,0 +1,21 @@
+package ru.netologe.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanechoTest {
+    @Test
+    void shouldReturnData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo ("some data"));
+
+    }
+}
